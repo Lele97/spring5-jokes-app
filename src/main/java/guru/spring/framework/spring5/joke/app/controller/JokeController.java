af56ca31d8b1e3e640ea6920da227a9d37e6b860
Lele97 <gabriele.grandinetti@hotmail.comcd>
2023-09-13 18:10:08 +0200

    commit Assignment
    add joke service class
    add joke controller class
    add view to show the joke
    add custom banner

---
+++ src/main/java/guru/spring/framework/spring5/joke/app/controller/JokeController.java
@@ -0,0 +1,27 @@
+package guru.spring.framework.spring5.joke.app.controller;
+
+import guru.spring.framework.spring5.joke.app.service.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/joke")
+public class JokeController {
+
+    @Autowired
+    private JokeService jokeService;
+
+    @GetMapping("/getjoke")
+    public String getJoke(Model model){
+        model.addAttribute("textJoke", jokeService.getJoke());
+        model.addAttribute("text","Message.");
+    return "index";
+    }
+
+
+
+
+}
